d6305b0e1d661c74a6e8ee7ce5734db59b6724f5
goohong <goohong.chung@gmail.com>
2025-02-27 17:45:25 +0900

    feat: 댓글 기능 구현

--- js/index.js
+++ js/index.js
@@ -1,19 +1,36 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+document.addEventListener("DOMContentLoaded", () => {
+    const commentForm = document.querySelector("form");
+    const commentInput = document.querySelector("textarea");
+    const commentList = document.querySelector(".comment-list");
 
-console.log("알맞은 스크립트를 작성하세요");
+    commentForm.addEventListener("submit", (event) => {
+        event.preventDefault(); // 기본 폼 제출 이벤트 방지
+        const commentText = commentInput.value.trim();
+
+        if (commentText === "") {
+            alert("댓글을 입력해주세요!");
+            return;
+        }
+
+        // 새로운 댓글 요소 생성
+        const commentItem = document.createElement("li");
+        commentItem.innerHTML = `
+            <div class="comment-item">
+                <div class="comment-author">
+                    <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+                    <span>방문자</span>
+                </div>
+                <div class="comment-content">${commentText}</div>
+            </div>
+        `;
+
+        // 목록에 추가
+        commentList.appendChild(commentItem);
+
+        // 성공 메시지
+        alert("댓글이 등록되었습니다!");
+
+        // 입력창 초기화
+        commentInput.value = "";
+    });
+});
